3a9587006e276861ee551331b45d7d85ef327ab1
Elisa holder <anglechilds751@gmail.com>
2018-08-01 18:27:29 +0000

    Portfolio

---
+++ script.js
@@ -0,0 +1,22 @@
+/* global $ */
+// JavaScript File
+$("#aboutme").click(function(){
+   alert("Im into dancing and im very committed to school ."); 
+   
+    
+    
+    
+});
+('#contact').click(function(){
+    alert("Number:6462504502 Gmail:elisaholder646@gmail.com");
+    
+    
+});
+('#click').click(function(){
+    alert("I picked this picture because it fits me. Ive always lived in Brooklyn but my favorite place was always the city. Especially at night , where everything lights up");
+    
+    
+    
+    
+});
+
